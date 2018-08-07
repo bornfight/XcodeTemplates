d9e281d0bdc267a7d8098e5be277fef178d64aed
Dino <dino.srdoc@degordian.com>
2018-08-07 10:24:02 +0200

    replace view with presenter; add header

---
+++ templates/RxMVVM.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+
+import UIKit
+
+class ___VARIABLE_sceneName___Presenter: BasePresenter {
+    
+}
--- templates/RxMVVM.xctemplate/___FILEBASENAME___View.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-class ___VARIABLE_sceneName___View: UIView {
-    
-}
