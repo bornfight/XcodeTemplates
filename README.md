65243e33af40afc9c7bcc23ef2602bc71f197765
Tamara Milisa <tamara.milisa@degordian.com>
2018-09-25 15:09:03 +0200

    add RealmObject template

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This project contains several Xcode templates.
 * The BaseCoconut template creates base classes for Coconut architecture.
 * The UserStorage template creates a class for storing data in UserDefaults.
 * The NavigationService template creates a class that handles transitions between screens and general app navigation.
+* The RealmObject template creates a class for Realm object.
 
 ## Installation
 
---
+++ templates/RealmObject.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import RealmSwift
+
+class ___VARIABLE_sceneName___: Object, Decodable {
+	
+	@objc dynamic var id: Int = 0
+
+	enum CodingKeys: String, CodingKey {
+		case id
+	}
+
+	required convenience init(from decoder: Decoder) throws {
+		self.init()
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		id = (try? container.decode(Int.self, forKey: .id)) ?? 0
+	}
+
+	convenience init(id: Int) {
+		self.init()
+		self.id = id
+	}
+
+}
