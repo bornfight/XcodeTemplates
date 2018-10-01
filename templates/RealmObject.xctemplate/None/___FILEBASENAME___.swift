725bec32af2ed793b60e9b134eeddd40e0149cca
Tamara Milisa <tamara.milisa@degordian.com>
2018-10-01 09:35:29 +0200

    fix realmObject template

---
+++ templates/RealmObject.xctemplate/None/___FILEBASENAME___.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import RealmSwift
+
+class ___VARIABLE_sceneName___: Object, Decodable {
+	
+	@objc dynamic var id: Int = 0
+
+	convenience init(id: Int) {
+		self.init()
+		self.id = id
+	}
+
+}
