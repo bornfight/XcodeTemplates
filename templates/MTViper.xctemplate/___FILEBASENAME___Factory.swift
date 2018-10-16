11b97f805927fc73f2cbe202220a9e3bec29f165
Tamara Milisa <tamara.milisa@degordian.com>
2018-10-16 10:21:32 +0200

    add new MT VIPER template

---
+++ templates/MTViper.xctemplate/___FILEBASENAME___Factory.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___VARIABLE_sceneName___Factory {
+
+	  //TODO: connect necesarry wireframe and use correct service
+//    static func createWithStoryboard(_ storyboard: UIStoryboard, wireframe: "wireframeName") -> SignupViewController {
+//        let view: ___VARIABLE_sceneName___ViewController = storyboard.instantiateViewController()
+//        let *some*Service = "someService()"
+//        let interactor = ___VARIABLE_sceneName___Interactor(*someService*: *some*Service)
+//        let presenter = ___VARIABLE_sceneName___Presenter(view: view, interactor: interactor, wireframe: wireframe)
+//        
+//        view.presenter = presenter
+//        interactor.presenter = presenter
+//        
+//        return view
+//    }
+
+
+}
---
+++ templates/MTViper.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___VARIABLE_sceneName___ViewController: UIViewController {
+
+    weak var presenter: ___VARIABLE_sceneName___Presenter!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
