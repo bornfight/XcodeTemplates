45e39ee1ee4725535f0d80502266bb6f2fdea97b
Josip Busak <josip.busak@bornfight.com>
2019-09-23 13:54:50 +0200

    add template with navigator

---
+++ templates/Coconut.xctemplate/ModelControllerNavigator/___FILEBASENAME___Navigator.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___VARIABLE_sceneName___Navigator: BaseNavigator {
+    enum Route: Navigatable {
+        
+    }
+    
+	weak var source: ___VARIABLE_sceneName___ViewController!
+
+    required init(source: ___VARIABLE_sceneName___ViewController) {
+        self.source = source
+    }
+    
+    func navigate(to destination: Route) {
+        switch destination {
+            
+        }
+    }
+}
